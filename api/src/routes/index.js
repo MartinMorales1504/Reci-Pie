0365250d0551640273e445be14ceb534993b49ec
MartinMorales1504 <martingabrielmorales@yahoo.com>
2023-01-06 20:04:03 -0300

    add router,use users

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,13 +1,13 @@
 const { Router } = require('express');
 const recipes = require('./recipes/recipes')
-
+const users = require('./users/users')
 
 const router = Router();
 
 // Configurar los routers
 
 router.use('/recipes', recipes);
-
+router.use('/users', users)
 
 
 module.exports = router;
