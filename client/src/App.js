632e4e74650631f6ddcc6fed4fcd406e858a316f
MartinMorales1504 <martingabrielmorales@yahoo.com>
2023-02-03 11:42:34 -0300

    slider applied in Login screen

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,15 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import Login from "./components/Login/Login";
-
+import Register from "./components/Register/Register"
+import Slider from "./components/Slider/Slider";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<Login/>}  />
+        <Route exact path="/register" element={<Register/>}  />
       </Routes>
     </div>
   );
