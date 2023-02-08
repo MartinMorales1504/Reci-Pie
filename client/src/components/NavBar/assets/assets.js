739acde1d4ee657eac3887a9d355bc6e711faf81
MartinMorales1504 <martingabrielmorales@yahoo.com>
2023-02-08 12:39:07 -0300

    Nav base

---
+++ client/src/components/NavBar/assets/assets.js
@@ -0,0 +1,9 @@
+import add from './add.png'
+import explore from './brujula.png'
+import search from './busqueda.png'
+import home from './home.png'
+import userImg from './user.png'
+import logOut from './LogOut.png'
+import logo from './Logo.png'
+ 
+export const images = {add, explore, search, home, userImg, logOut, logo}
