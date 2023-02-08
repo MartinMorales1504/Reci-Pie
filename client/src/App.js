1fcd0c53c63c40ff8f480d76f855440242e24eb0
MartinMorales1504 <martingabrielmorales@yahoo.com>
2023-02-08 12:40:56 -0300

    general changes

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register"
-import Slider from "./components/Slider/Slider";
+import Explore from "./components/Explore/Explore";
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Login/>}  />
         <Route exact path="/register" element={<Register/>}  />
+        <Route exact path="/explore" element={<Explore/>}  />
       </Routes>
     </div>
   );
---
+++ client/src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit"
+// import users from "./userSlice.js"
+
+export default configureStore({
+    reducer:{
+        // users: users,
+    }
+})
